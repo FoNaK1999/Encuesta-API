e9d62e963f0ff60b68cbc348728bdf3cd731e501
Martin Vivanco <martintomasvivancomella@gmail.com>
2021-12-05 14:59:06 -0300

    Variables de entorno

--- src/bd/conexion.js
+++ src/bd/conexion.js
@@ -1,11 +1,16 @@
+require('dotenv').config();
 const {Pool} = require('pg');
 
 const pool = new Pool({
-    user: "postgres",
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.PORTSQL
+})
+    /*user: "postgres",
     host: "localhost",
     database: "sistema-encuestas",
     password: "admin",
-    port: 5432,
-})
-
+    port: 5432,*/
 module.exports = pool;
